c10bc9a7b5df5070b14023c39fbc868d664cb3fa
Alexander Chen <alex7c@gmail.com>
2012-10-26 01:39:50 -0700

    Add folders back

--- run.py
+++ run.py
@@ -1,5 +1,33 @@
 import os
-from Authoring import app
+from flask import Flask, render_template, flash, redirect, url_for
+from flask.ext.wtf import Form, TextField, Required, TextAreaField
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'somesecretekey')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+Bootstrap(app)
+
+class QuestionForm(Form):
+    xml = TextAreaField('XML code')
+
+@app.route("/")
+def hello():
+    return render_template('hello.html')
+
+@app.route("/questions")
+def listQuestions():
+    return render_template('hello.html')
+
+@app.route("/question/new", methods=['GET', 'POST'])
+def newQuestion():
+    form = QuestionForm()
+    if form.validate_on_submit():
+        flash("submitted!")
+        return redirect(url_for('newQuestion'))
+    else:
+        return render_template('newQuestion.html', form=form)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
