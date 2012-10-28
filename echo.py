c23d67153abd84d4e4b9b9f86033589d97a9ba40
Alexander Chen <alex7c@gmail.com>
2012-10-27 23:25:06 -0700

    Removing echo server

--- echo.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route("/", methods=['GET', 'POST', 'DELETE', 'PUT'])
-def echo():
-    return request.data
-
-if __name__ == "__main__":
-    app.run(port=4000)
